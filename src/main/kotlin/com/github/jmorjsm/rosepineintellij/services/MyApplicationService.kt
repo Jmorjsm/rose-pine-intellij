07810aff54948e9b144275f125afdf2fceb4a136
GitHub Action <action@github.com>
2021-12-04 11:15:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/jmorjsm/rosepineintellij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jmorjsm/rosepineintellij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jmorjsm.rosepineintellij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jmorjsm.rosepineintellij.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/jmorjsm/rosepineintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jmorjsm/rosepineintellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jmorjsm.rosepineintellij.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jmorjsm.rosepineintellij.MyBundle
 
 class MyProjectService(project: Project) {
 
